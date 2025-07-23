30cfff96f5201657f920a73ff6519e647c0312d6
jordan <jordanscheftic@yahoo.com>
2025-07-23 00:42:28 -0500

    nodemidware: false

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    nodeMiddleware: true,
-  },
+  // experimental: {
+  //   nodeMiddleware: true,
+  // },
   eslint: {
     ignoreDuringBuilds: true,
   },
